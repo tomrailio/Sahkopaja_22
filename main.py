d3b1c3994ec80aa8b60b5f2f0e1c637d2995ac1e
tomrailio <45072246+tomrailio@users.noreply.github.com>
2022-10-27 11:15:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,19 @@
 def main():
   while != ""         # Käynnistä looppi.
                       # kysy monitorista ... left, right jne. 
+    # define loop rate (example 1 second)
+    # tnow = current time
+    # check with sensor_monitor
+    # monitor_answer = monitor.process_signal()   # monitor says whether it detected something during this loop
+    # if monitor_response == 0:
+      # pass
+    # elif monitor__response == 1:
+    #    if response was positive, activate controller for LEDs / other relevant system
+    # before loop ends, wait until loop rate is done
+    # 
+    # while (endtime - tnow) < 1 second:
+    #     endtime = current time
+    #     wait
 
 
   
