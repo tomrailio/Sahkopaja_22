c34688f301d68612189a1c92d7b7a3d97cc9a269
jnlb <jn.lindblad@gmail.com>
2022-11-10 12:00:46 +0200

    some documentation for pocketsphinx demo

--- livespeech-background.py
+++ livespeech-background.py
@@ -1,4 +1,8 @@
 # this is a test of pocketsphinx live speech
+# recognizes words listed in 'keyphrases.txt' in background
+# pocketsphinx does not use an internet connection and may be faster than google
+# parameter kws_threshold controls recognition sensitivity, range of 1e-6 to 1e-10 seems OK
+# maxwpf=1 prevents the program from recognizing more than 1 word 
 
 from pocketsphinx import LiveSpeech
 
